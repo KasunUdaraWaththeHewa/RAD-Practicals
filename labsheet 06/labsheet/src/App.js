e3ff9d1675599cb4fd33ce9402f23956f08cfdde
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-19 12:20:30 +0530

    ToDoList done

--- labsheet 06/labsheet/src/App.js
+++ labsheet 06/labsheet/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Counter from './Counter';
 import Toggle from './Toggle';
 import Timer from './Timer';
+import TodoList, { todos } from './TodoList'; 
+
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <TodoList todos={todos} />
     </div>
   );
 }
