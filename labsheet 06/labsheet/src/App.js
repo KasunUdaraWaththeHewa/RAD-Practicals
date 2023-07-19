47e7ba0b4e5988afa626bf320becb599f6801d75
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-19 12:25:25 +0530

    CardList Done

--- labsheet 06/labsheet/src/App.js
+++ labsheet 06/labsheet/src/App.js
@@ -4,12 +4,13 @@ import Counter from './Counter';
 import Toggle from './Toggle';
 import Timer from './Timer';
 import TodoList, { todos } from './TodoList'; 
+import CardList from './CardList';
 
 
 function App() {
   return (
     <div className="App">
-      <TodoList todos={todos} />
+      <CardList />
     </div>
   );
 }
