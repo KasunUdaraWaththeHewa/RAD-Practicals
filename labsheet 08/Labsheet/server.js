dc2447745a32cfbbfae70333ca93a07aa9c9408e
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-09 20:14:22 +0530

    first problem done

--- labsheet 08/Labsheet/server.js
+++ labsheet 08/Labsheet/server.js
@@ -5,6 +5,11 @@ app.get('/',(req,res)=>{
     res.send("Successful Response");
 });
 
+app.get('/greet/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello, ${name}!`);
+});
+
 const port=3000;
 app.listen(port,()=>{
     console.log(`Server is listening at http://localhost:${port}`);
